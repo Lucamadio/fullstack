2d037f2d06c630e2c0f999e31da626d43973b120
Luca Amadio <lucamadio@hotmail.it>
2018-11-18 10:37:24 +0100

    Added Logger with Logback

--- src/main/java/com/popper/fullstack/web/i18n/I18NService.java
+++ src/main/java/com/popper/fullstack/web/i18n/I18NService.java
@@ -2,6 +2,8 @@ package com.popper.fullstack.web.i18n;
 
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
@@ -9,14 +11,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class I18NService {
 
+	private static final Logger LOG = LoggerFactory.getLogger(I18NService.class);
+	
 	private MessageSource messageSource;
 	
 	/**
 	 * Return a message for the given message id and the default locale
 	 * in the session context
-	 * @param messageId The key to hte messages resource file
+	 * @param messageId The key to the messages resource file
 	 * **/
 	public String getMessage(String messageId){
+		LOG.info("Returning i18n text for messageId: {})", messageId);
 		Locale locale = LocaleContextHolder.getLocale();
 		return getMessage(messageId, locale);
 	}
@@ -24,7 +29,7 @@ public class I18NService {
 	/**
 	 * Return a message for the given message id and the default locale
 	 * in the session context
-	 * @param messageId The key to hte messages resource file
+	 * @param messageId The key to the messages resource file
 	 * @param locale The localeS
 	 * **/
 	private String getMessage(String messageId, Locale locale) {
